c50d1ca89d82de9de5106df6877b629a39384fff
isac <isacsouzacampos@hotmail.com>
2019-12-04 15:25:49 -0300

    ...

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
     
     // int linhas = 10, colunas = 10;
     Grafo gr = Grafo();
-    int ordem_plano = 10;
+    int ordem_plano = 30;
     int linhas = ordem_plano, colunas = ordem_plano;
 
     for(int i = 0; i < linhas*colunas; i++) {
@@ -38,35 +38,6 @@ int main(int argc, char* argv[]) {
     t_fim = clock();
     t_decorrido = ((t_fim - t_inicio) / (CLOCKS_PER_SEC / 1000));
     cout << "tempo = " << t_decorrido << endl;
-
-    // tornar todos os vertices bloqueados
-    // for(auto& mc : gr.map_cor)
-    //     mc.second = color::PRETO;
-
-    // for(int ordem = 3; ordem < ordem_plano; ordem++) {
-    //     gr.atualizarOrdem(ordem);
-
-    //     // gerar caminhos
-    //     t_inicio = clock();
-
-    //     float centro = (ordem/2)-0.5;
-    //     centro += (centro*ordem)+ordem;
-    //     centro -= (ordem/2)-0.5;
-    //     float inferior_esquerdo = ordem*(ordem-1), superior_direito = ordem-1, inferior_direito = (ordem*ordem)-1;
-    //     gr.maze(0, inferior_esquerdo);
-    //     gr.maze(superior_direito, inferior_direito);
-    //     gr.maze(centro, superior_direito);
-    //     gr.maze(centro, 0);
-    //     gr.maze(centro, inferior_direito);
-
-    //     t_fim = clock();
-
-    //     t_decorrido = ((t_fim - t_inicio) / (CLOCKS_PER_SEC / 1000));
-    //     // t_decorrido = ((float) (t_fim - t_inicio) ) / CLOCKS_PER_SEC;
-        // myfile << ordem << ","<< t_decorrido << endl;
-    //     cout << "para ordem " << ordem << ", tempo = " << t_decorrido << endl;
-    //     ordem++;
-    // }
 
     int opcao = -1;
     while(opcao) {
